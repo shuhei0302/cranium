4c7a651a9e6c5405f241e13e363447e9f57768c5
Shuhei <a.f.z.v.94@gmail.com>
2021-09-25 18:57:09 +0900

    top画面作成

--- src/main/java/com/example/demo/TopController.java
+++ src/main/java/com/example/demo/TopController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TopController {
 	@RequestMapping(value="/top")
 	private String index(){
-		return "/index.html";
+		return "/top.html";
 	}
 }
